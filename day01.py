46272ae408768d4bada4f0c01789f4cef60c0fe0
Fritz Reichwald <mail@fritzreichwald.de>
2017-12-06 21:21:01 +0100

    Also fix dady01 and day02

--- day01.py
+++ day01.py
@@ -1,33 +1,67 @@
 #!/usr/bin/python3
 import sys
 from datetime import datetime
-import time
+import timeit
 
-before = datetime.now()
-with open(sys.argv[1], 'r') as f:
-    input = f.read()
+# Global variables
+result01 = 0
+result02 = 0
+input = ''
 
 #part 1
-sum = 0
-i = 0
-while i < len(input):
-    if (i < len(input) - 1):
-        if (input[i] == input[i+1]):
-            sum += int(input[i])
-    else:
-        if (input[i] == input[0]):
-            sum += int(input[i])
-    i += 1
-after = datetime.now()
-print('Solution 1:', sum)
-print('Duration:', after-before)
+def part01():
+    global result01
+    global input
+    sum = 0
+    i = 0
+    while i < len(input):
+        if (i < len(input) - 1):
+            if (input[i] == input[i+1]):
+                sum += int(input[i])
+        else:
+            if (input[i] == input[0]):
+                sum += int(input[i])
+        i += 1
+    result01 = sum
+
 
 #part 2
-#sum = 0
-#i = 0
-#l = len(input)
-#while i < l:
-#    if (input[i] == input[int((i+(l/2))%l)]):                                   
-#        sum += int(input[i])
-#    i += 1
-#print('Solution 2:', sum)
+def part02():
+    global result02
+    global input
+    sum = 0
+    i = 0
+    l = len(input)
+    while i < l:
+        if (input[i] == input[int((i+(l/2))%l)]):                                   
+            sum += int(input[i])
+        i += 1
+    result02 = sum
+
+
+def bench(part=0, filename=''):
+    global input
+    input = ''
+    if filename != '':
+        with open(filename, 'r') as f:
+           input = f.readline() 
+
+        if part == 1:
+            duration01 = timeit.timeit("part01()", setup="from day01 import part01", number=1)
+            print(1, 1, result01, int(duration01 * 10 ** 6))
+        elif part == 2:
+            duration02 = timeit.timeit("part02()", setup="from day01 import part02", number=1)
+            print(1, 2, result02, int(duration02 * 10 ** 6)) 
+            
+
+# Main
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        input = f.readline()
+
+    duration01 = timeit.timeit("part01()", setup="from __main__ import part01", number=1)
+    print(1, 1, result01, int(duration01 * 10 ** 6))
+
+    duration02 = timeit.timeit("part02()", setup="from __main__ import part02", number=1)
+    print(1, 2, result02, int(duration02 * 10 ** 6)) 
+
--- day02.py
+++ day02.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 import sys
 from datetime import datetime
-import time
+import timeit
 
-before = datetime.now()
-sum = 0
-with open(sys.argv[1], 'r') as f:
-    for line in f:
+spreadlist = []
+
+def part01():
+    global spreadlist
+    global result01
+    sum = 0
+    for spreadline in spreadlist:
         lowest = 0
         highest = 0
-        spreadline = line.split('\t')
         for number in spreadline:
             inumber = int(number)
             if (lowest == 0 and highest == 0):
@@ -21,15 +23,13 @@ with open(sys.argv[1], 'r') as f:
                 lowest = inumber
         
         sum += highest - lowest
-after = datetime.now()
-print('Solution 1:', sum)
-print('Duration:', after-before)
+    result01 = sum
 
-before = datetime.now()
-sum = 0
-with open(sys.argv[1], 'r') as f:
-    for line in f:
-        spreadline = line.split('\t')
+def part02():
+    global spreadlist
+    global result02
+    sum = 0
+    for spreadline in spreadlist:
         l = len(spreadline)
         i = 0
         while i < l:
@@ -48,7 +48,33 @@ with open(sys.argv[1], 'r') as f:
                 else:
                     k += 1
             i += 1
+    result02 = int(sum)
 
-after = datetime.now()
-print('Solution 2:', sum)
-print('Duration:', after-before)
+def bench(part=0, filename=''):
+    global spreadlist
+    spreadlist = []
+    if filename != '':
+        with open(filename, 'r') as f:
+            for line in f:
+                spreadlist.append(line.split('\t'))
+
+        if part == 1:
+            duration01 = timeit.timeit("part01()", setup="from day02 import part01", number=1)
+            print(2, 1, result01, int(duration01 * 10 ** 6))
+        elif part == 2:
+            duration02 = timeit.timeit("part02()", setup="from day02 import part02", number=1)
+            print(2, 2, result02, int(duration02 * 10 ** 6)) 
+            
+
+# Main
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f:
+        for line in f:
+            spreadlist.append(line.split('\t'))
+
+    duration01 = timeit.timeit("part01()", setup="from __main__ import part01", number=1)
+    print(2, 1, result01, int(duration01 * 10 ** 6))
+
+    duration02 = timeit.timeit("part02()", setup="from __main__ import part02", number=1)
+    print(2, 2, result02, int(duration02 * 10 ** 6)) 
+
